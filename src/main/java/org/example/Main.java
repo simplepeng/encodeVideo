2fb1c43b5c037f8a7c32387c2baa20baeb02c9ed
simple <383559698@qq.com>
2022-09-28 23:49:14 +0800

    add imgs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,24 @@ package org.example;
 
 import org.bytedeco.javacv.FFmpegFrameRecorder;
 
+import java.io.File;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        String path = "";
+    public static void main(String[] args) throws FFmpegFrameRecorder.Exception {
+        File root = new File("");
+        System.out.println("root = " + root.getAbsolutePath());
+
+    }
+
+    public static void encodeToVideo(File root) {
+
+    }
+
+    public static void encodeToVideo(List<File> files, String outPath) throws Throwable {
         int width = 0;
         int height = 0;
-        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(path, width, height);
+        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(outPath, width, height);
+        recorder.release();
     }
 }
